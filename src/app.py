e73c8fdea12dd697cac8889e305236ebcdbd3edb
samiullah <sami606713@gmail.com>
2024-01-22 18:42:09 +0500

    Basic Chat bot done

--- src/app.py
+++ src/app.py
@@ -1,29 +1,38 @@
 import streamlit as st
 from utils import Response
-def main():
-    with st.sidebar.container():
-        st.title("history")
 
+# Initialize the chat history in session state
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+def main():
+    # Set the main container for chat input
     with st.container():
         st.title("Chat Gpt Clone")
 
     # make a empty chat list that can store the chat 
-    chat_history=[]
     user_input=st.chat_input("Enter your text: ")
-    if (user_input):
+    if(user_input):
         st.write(user_input)
-        chat_history.append(user_input)
-        with st.status("bot responding...."):
+        
+        with st.spinner("bot responding...."):
             response=Response(user_input)
-        chat_history.append(response)
-        st.write(response)
+        # st.write(response) 
+        # Store the user input and resposne in sessin state
+        st.session_state.chat_history.append(
+            {
+                "user":user_input,
+                "bot":response
+            }   
+        )
         
-        st.empty()
-        for message in chat_history:
-            st.write(message)
-        
-
-    def Chat_Histort():
-        pass
+        # Fetch result in session state and display the result
+        for entry in st.session_state.chat_history:
+            st.write(f"Bot: {entry['user']}")
+            st.write(f"Bot: {entry['bot']}")
+            st.empty()
+    
+    # Set the container for history
+    with st.sidebar.container():
+        st.title("history")
 if __name__=="__main__":
     main()
