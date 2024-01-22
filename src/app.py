08e5bc6a63b6e056ed723ebc00452fc1cb9776d0
samiullah <sami606713@gmail.com>
2024-01-22 19:26:59 +0500

    Basic Chat bot done

--- src/app.py
+++ src/app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 from utils import Response
+from PIL import Image
 
 # Initialize the chat history in session state
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
+
+
 def main():
     # Set the main container for chat input
     with st.container():
@@ -11,12 +14,14 @@ def main():
 
     # make a empty chat list that can store the chat 
     user_input=st.chat_input("Enter your text: ")
+    # uploaded_image = st.file_uploader("Upload an image:", type=["jpg", "jpeg", "png"])
+
     if(user_input):
-        st.write(user_input)
+        st.write(user_input) 
         
         with st.spinner("bot responding...."):
             response=Response(user_input)
-        # st.write(response) 
+
         # Store the user input and resposne in sessin state
         st.session_state.chat_history.append(
             {
@@ -27,8 +32,11 @@ def main():
         
         # Fetch result in session state and display the result
         for entry in st.session_state.chat_history:
-            st.write(f"Bot: {entry['user']}")
-            st.write(f"Bot: {entry['bot']}")
+            with st.chat_message("user"):
+                st.write(f"{entry['user']}")
+            
+            with st.chat_message("assistant"):
+                st.write(f"{entry['bot']}")
             st.empty()
     
     # Set the container for history
